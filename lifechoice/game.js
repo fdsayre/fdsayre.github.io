1d3f6f19c41fdeb7bb44b4a911b05d98ea2fba64
Franklin Sayre <frank.darrow@gmail.com>
2023-05-13 09:42:30 -0700

    Add files via upload

--- lifechoice/game.js
+++ lifechoice/game.js
@@ -1,23 +1,27 @@
 /* game.js */
 
-let score = 0;
-let round = 1;
-let turn = 0;
-let totalTurns = 10;
-let nextRoundButton = document.getElementById('next-round');
-let skyImage = document.getElementById('sky-image');
+document.addEventListener("DOMContentLoaded", function() {
+    let score = 0;
+    let round = 1;
+    let turn = 0;
+    let totalTurns = 10;
+    let nextRoundButton = document.getElementById('next-round');
+    let skyImage = document.getElementById('sky-image');
 
-// Array of emojis representing various life events
-let emojis = ["🌳", "👪", "💼", "🎉", "😊", "🌧️", "🤒", "🚗", "🌈", "🌪️"];
+    // Array of emojis representing various life events
+    let emojis = ["🌳", "👪", "💼", "🎉", "😊", "🌧️", "🤒", "🚗", "🌈", "🌪️"];
 
-document.getElementById('take-photo').onclick = increaseScore;
-document.getElementById('check-email').onclick = increaseScore;
-document.getElementById('meditate').onclick = increaseScore;
-document.getElementById('eat-ice-cream').onclick = increaseScore;
+    document.getElementById('take-photo').onclick = increaseScore;
+    document.getElementById('check-email').onclick = increaseScore;
+    document.getElementById('meditate').onclick = increaseScore;
+    document.getElementById('eat-ice-cream').onclick = increaseScore;
 
-nextRoundButton.onclick = nextRound;
-document.getElementById('end-game').onclick = endGame;
+    nextRoundButton.onclick = nextRound;
+    document.getElementById('end-game').onclick = endGame;
 
+    // Add dark mode button event handler
+    document.getElementById('toggle-dark-mode').onclick = toggleDarkMode;
+    
 function increaseScore() {
     if(turn < totalTurns) {
         score++;
@@ -57,6 +61,16 @@ function resetGame() {
     skyImage.innerText = randomEmoji;
 }
 
+document.getElementById('toggle-dark-mode').onclick = function() {
+    if (document.body.classList.contains('dark-mode')) {
+        document.body.classList.remove('dark-mode');
+        document.body.classList.add('light-mode');
+    } else {
+        document.body.classList.remove('light-mode');
+        document.body.classList.add('dark-mode');
+    }
+}
 // Set initial emoji
-let randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
-skyImage.innerText = randomEmoji;
+    let randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+    skyImage.innerText = randomEmoji;
+});
